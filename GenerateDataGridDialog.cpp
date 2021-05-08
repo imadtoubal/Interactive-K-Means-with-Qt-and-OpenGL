8dab357f76d95a33ca3f217af7364c60904d2865
Imad Toubal <imad.toubal@gmail.com>
2021-05-08 12:18:16 -0500

    Fixes to GenerateGridDialog
    The dialog UI and logic for generating a 3D data grid is fixed.

--- GenerateDataGridDialog.cpp
+++ GenerateDataGridDialog.cpp
@@ -1,11 +1,14 @@
 #include "GenerateDataGridDialog.h"
 #include "ui_GenerateDataGridDialog.h"
+#include <QDebug>
 
 GenerateDataGridDialog::GenerateDataGridDialog(QWidget *parent) :
   QDialog(parent),
-  ui(new Ui::GenerateDataGridDialog)
-{
+  ui(new Ui::GenerateDataGridDialog) {
   ui->setupUi(this);
+
+  connect(this, &GenerateDataGridDialog::accepted,
+          this, &GenerateDataGridDialog::apply);
 }
 
 GenerateDataGridDialog::~GenerateDataGridDialog() {
@@ -22,4 +25,23 @@ void GenerateDataGridDialog::setMinY(double y) {
 
 void GenerateDataGridDialog::setMinZ(double z) {
   ui->minBoundZSpinBox->setValue(-z);
+}
+
+void GenerateDataGridDialog::apply() {
+
+  float minX = ui->minBoundXSpinBox->value();
+  float minY = ui->minBoundYSpinBox->value();
+  float minZ = ui->minBoundZSpinBox->value();
+
+  float maxX = ui->maxBoundXSpinBox->value();
+  float maxY = ui->maxBoundYSpinBox->value();
+  float maxZ = ui->maxBoundZSpinBox->value();
+
+  float stepX = ui->stepXSpinBox->value();
+  float stepY = ui->stepYSpinBox->value();
+  float stepZ = ui->stepZSpinBox->value();
+
+  emit dataParametersChanged(minX, minY, minZ,
+                             maxX, maxY, maxZ,
+                             stepX, stepY, stepZ);
 }
--- GenerateDataGridDialog.h
+++ GenerateDataGridDialog.h
@@ -15,10 +15,17 @@ public:
   explicit GenerateDataGridDialog(QWidget *parent = nullptr);
   ~GenerateDataGridDialog();
 
+signals:
+  void dataParametersChanged(float, float, float,
+                             float, float, float,
+                             float, float, float);
+
 public slots:
   void setMinX(double x);
   void setMinY(double y);
   void setMinZ(double z);
+
+  void apply();
 
 private:
   Ui::GenerateDataGridDialog *ui;
